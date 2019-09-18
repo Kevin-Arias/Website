7d50ae923daedb90ca7ae228625fb8455d7f79ff
Kevin9456 <kevin.arias@berkeley.edu>
2019-09-17 23:25:57 -0700

    Recent Commit

--- mail.php
+++ mail.php
@@ -1,27 +1,19 @@
 <?php
     $to = 'kevinarias.eecs@gmail.com';
-    $firstname = $_POST["fname"];
+    $name = $_POST["name"];
     $email= $_POST["email"];
     $text= $_POST["message"];
-    $phone= $_POST["phone"];
+    $subject= $_POST["subject"];
     
 
+    $message = "Name: ".$name."\n\n".$text;
 
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers = "From: " . $email; // Sender's E-mail
+    
 
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$phone.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
-        
-    </table>';
+    
 
-    if (mail($to, $email, $message, $headers))
+    if (mail($to, $subject, $message, $headers))
     {
         echo 'The message has been sent.';
     }else{
